bdbb3b7980fe1412a8c4fe98414203b591089644
alrafi34 <saifullahrafi345@gmail.com>
2026-03-16 17:54:12 +0600

    feat: Integrate Vercel Analytics for website tracking.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins, Inter } from "next/font/google";
 import "./globals.css";
 import { siteConfig } from "@/config/site";
+import { Analytics } from "@vercel/analytics/next"
 import NavigationProvider from "@/components/NavigationProvider";
 
 const poppins = Poppins({
@@ -85,6 +86,7 @@ export default function RootLayout({
         <NavigationProvider>
           {children}
         </NavigationProvider>
+        <Analytics />
       </body>
     </html>
   );
