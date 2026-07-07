3ae339989d2eb7af34452a58a76325129f0f37ea
Rafi personal <rafipersonal@Saifullahs-Mac-mini.local>
2026-07-07 08:29:01 +0600

    Update sitemap config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.productivetoolbox.com" }],
-        destination: "https://productivetoolbox.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
   experimental: {
     optimizePackageImports: ["lucide-react"],
   },
